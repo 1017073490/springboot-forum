d3b0d957f593c61b9e634f1bfc446d75e4d0a545
zhangxing <1017073490@qq.com>
2020-11-08 17:40:41 +0800

    init repo 1

---
+++ src/main/java/com/zhangxing/springbootforum/SpringbootForumApplication.java
@@ -0,0 +1,13 @@
+package com.zhangxing.springbootforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootForumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhangxing/springbootforum/SpringbootForumApplicationTests.java
@@ -0,0 +1,14 @@
+package com.zhangxing.springbootforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("success");
+    }
+
+}

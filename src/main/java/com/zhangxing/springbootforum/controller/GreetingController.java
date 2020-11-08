df8a03e04fd730788acf99e300b19995b64502dd
zhangxing <1017073490@qq.com>
2020-11-08 19:46:14 +0800

    add index and bootstrap

--- src/main/java/com/zhangxing/springbootforum/controller/GreetingController.java
+++ src/main/java/com/zhangxing/springbootforum/controller/GreetingController.java
@@ -3,7 +3,6 @@ package com.zhangxing.springbootforum.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
@@ -14,10 +13,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false,defaultValue = "World")
-                                   String name, Model model) {
-        model.addAttribute("name",name);
-        return "greeting";
+    @GetMapping("/")
+    public String greeting() {
+        return "index";
     }
 }
